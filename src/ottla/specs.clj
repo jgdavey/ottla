e2f616965d39cb3110cb94b14469f3c51a2b0402
Joshua Davey <josh@joshuadavey.com>
2026-03-21 16:15:53 -0400

    Fix specs

--- src/ottla/specs.clj
+++ src/ottla/specs.clj
@@ -18,6 +18,8 @@
                                                [1 (gen/return \_)]])
                                0 41))))
 
+;;; Config
+
 (s/def :ottla.config/conn-map map?)
 (s/def :ottla.config/schema (s/with-gen postgres/legal-identifier?
                               sql-identifier-gen))
@@ -36,12 +38,15 @@
          (dissoc (get args :config) :conn)
          (dissoc ret :conn))))
 
+;;; Topics
+
 (s/def :ottla.topic/topic (s/and string?
                                  #(pos? (count %))))
 (s/def :ottla.topic/table-name (s/and string?
                                       #(pos? (count %))))
 (s/def :ottla.topic/key-type #{:text :bytea :jsonb})
 (s/def :ottla.topic/value-type #{:text :bytea :jsonb})
+(s/def :ottla.topic/index-key? boolean?)
 
 (s/def :ottla/topic-map (s/keys :req-un [:ottla.topic/topic
                                          :ottla.topic/key-type
@@ -51,12 +56,28 @@
 (s/fdef ottla/add-topic!
   :args (s/cat :config :ottla/config
                :ottla.topic :ottla.topic/topic
-               :opts (s/keys* :opt-un [:ottla.topic/key-type :ottla.topic/value-type]))
+               :opts (s/keys* :opt-un [:ottla.topic/key-type
+                                       :ottla.topic/value-type
+                                       :ottla.topic/index-key?]))
+  :ret :ottla/topic-map)
+
+(s/fdef ottla/ensure-topic
+  :args (s/cat :config :ottla/config
+               :topic :ottla.topic/topic
+               :opts (s/keys* :opt-un [:ottla.topic/key-type
+                                       :ottla.topic/value-type
+                                       :ottla.topic/index-key?]))
   :ret :ottla/topic-map)
 
 (s/fdef ottla/remove-topic!
   :args (s/cat :config :ottla/config
                :ottla.topic :ottla.topic/topic))
+
+(s/fdef ottla/list-topics
+  :args (s/cat :config :ottla/config)
+  :ret (s/coll-of :ottla/topic-map))
+
+;;; Records and append
 
 (def serialize-spec (s/with-gen
                       (s/or :kw keyword?
@@ -66,6 +87,7 @@
 (s/def :ottla.append/serialize-key serialize-spec)
 (s/def :ottla.append/serialize-value serialize-spec)
 
+(s/def :ottla.record/eid pos-int?)
 (s/def :ottla.record/key any?)
 (s/def :ottla.record/value any?)
 (s/def :ottla.record/meta (s/nilable (s/map-of keyword? any?)))
@@ -88,27 +110,50 @@
                :opts (s/keys* :opt-un [:ottla.append/serialize-key
                                        :ottla.append/serialize-value])))
 
+;;; Trim
+
+(s/def :ottla.trim/before-eid pos-int?)
+(s/def :ottla.trim/before-timestamp inst?)
+(s/def :ottla.trim/all? true?)
+(s/def :ottla.trim/ignore-subscriptions? boolean?)
+
+(s/fdef ottla/trim-topic!
+  :args (s/cat :config :ottla/config
+               :topic :ottla.topic/topic
+               :opts (s/keys* :opt-un [:ottla.trim/before-eid
+                                       :ottla.trim/before-timestamp
+                                       :ottla.trim/all?
+                                       :ottla.trim/ignore-subscriptions?]))
+  :ret nat-int?)
+
+;;; Selection and consumers
+
 (s/def :ottla.selection/topic :ottla.topic/topic)
 (s/def :ottla.selection/group string?)
-(s/def :ottla.selection/tx-mode #{:auto :manual :tx-wrap})
+(s/def :ottla.selection/commit-mode #{:auto :manual :tx-wrap})
 
-(s/def :ottla/selection (s/keys :req-un [:ottla.selection/topic]
-                                :opt-un [:ottla.selection/group
-                                         :ottla.selection/tx-mode]))
+(s/def :ottla/selection
+  (s/or :topic-name string?
+        :map (s/keys :req-un [:ottla.selection/topic]
+                     :opt-un [:ottla.selection/group
+                               :ottla.selection/commit-mode])))
 
 (s/def :ottla.consumer/poll-ms pos-int?)
 (s/def :ottla.consumer/await-close-ms pos-int?)
 (s/def :ottla.consumer/listen-ms pos-int?)
+(s/def :ottla.consumer/reconnect-ms pos-int?)
+(s/def :ottla.consumer/max-records pos-int?)
 (s/def :ottla.consumer/deserialize-key serialize-spec)
 (s/def :ottla.consumer/deserialize-value serialize-spec)
 (s/def :ottla.consumer/xform idfn)
 (s/def :ottla.consumer/exception-handler idfn)
 
-
 (def ^:private consumer-opt-keys
   [:ottla.consumer/poll-ms
    :ottla.consumer/await-close-ms
    :ottla.consumer/listen-ms
+   :ottla.consumer/reconnect-ms
+   :ottla.consumer/max-records
    :ottla.consumer/deserialize-key
    :ottla.consumer/deserialize-value
    :ottla.consumer/xform
@@ -125,3 +170,21 @@
                :selection :ottla/selection
                :handler fn?
                :opts :ottla/consumer-opts))
+
+(s/fdef ottla/commit-offset!
+  :args (s/cat :config :ottla/config
+               :selection :ottla/selection
+               :record (s/keys :req-un [:ottla.record/eid])))
+
+(s/fdef ottla/reset-consumer-offset!
+  :args (s/cat :config :ottla/config
+               :selection :ottla/selection
+               :new-offset nat-int?))
+
+;;; Monitoring
+
+(s/def :ottla.list-subscriptions/topics (s/coll-of string?))
+
+(s/fdef ottla/list-subscriptions
+  :args (s/cat :config :ottla/config
+               :opts (s/keys* :opt-un [:ottla.list-subscriptions/topics])))
